d99239f78e25b6d3090658688a5583eec19c823f
xin <xin@xindeMacBook-Air.local>
2018-09-01 21:25:08 +0800

    ajax 1

--- todo.js
+++ todo.js
@@ -1,3 +1,7 @@
 var log = console.log.bind(console)
 var e = sel => document.querySelector(sel)
 var appendHtml = (element, html) => element.insertAdjancentHTML('beforeend', html)
+var ajax = function(method, path, data, callback) {
+    var r = new XMLHttpRequest()
+    r.open(method, path, true)
+}
